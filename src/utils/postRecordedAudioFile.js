a510f194408695dc70b4ab4ad779c47cbaeb27a8
Raúl Pérez <jrpcassone@gmail.com>
2024-07-02 16:07:20 +0200

    Correct env variable

--- src/utils/postRecordedAudioFile.js
+++ src/utils/postRecordedAudioFile.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const localRoute = "http://127.0.0.1:5000/";
+export const localRoute = process.env.PUBLIC_URL;
 
 const postRecordedAudioFile = async (audioBlob) => {
   try {
